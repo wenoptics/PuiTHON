7384fb116318a5196826ccb968f9aca0ce631169
Grayson Wen <wenoptics@gmail.com>
2019-06-27 15:18:51 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='framework-puithon',
+    version='0.1.0',
+    packages=['example', 'puithon'],
+    include_package_data=True,
+    url='',
+    license='',
+    author='Grayson Wen',
+    author_email='wenoptics@gmail.com',
+    description='A framework for easy gluing HTML based frontend with Python backend, for desktop-based applications.'
+)
